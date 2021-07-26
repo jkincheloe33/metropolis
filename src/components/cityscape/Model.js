7460c5df8217e49ee96fdbf29c7120a922c03de2
Josh Kincheloe <jkincheloe33@gmail.com>
2021-07-26 08:31:41 -0500

    removed commented code

--- src/components/cityscape/Model.js
+++ src/components/cityscape/Model.js
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
 import { extend, useFrame, useThree } from '@react-three/fiber'
-import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -20,22 +19,11 @@ export const Controls = () => {
 
 const Model = ({ ...props }) => {
   const [model, setModel] = useState()
-  // const manager = new THREE.LoadingManager()
 
   useEffect(() => {
     new GLTFLoader().load('./obj/city/scene.gltf', setModel)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
-
-  //   useEffect(() => {
-  //     if (model) {
-  //       model.scene.traverse(child => {
-  //         if (child.material) {
-  //           setMaterial(child.material)
-  //         }
-  //       })
-  //     }
-  //   }, [model])
 
   return model ? <primitive {...props} object={model.scene} /> : null
 }
