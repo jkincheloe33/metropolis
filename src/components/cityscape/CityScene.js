62754a60f79148726274df8234f29063ab9ff83a
Josh Kincheloe <jkincheloe33@gmail.com>
2021-08-07 20:10:19 -0500

    new positions

--- src/components/cityscape/CityScene.js
+++ src/components/cityscape/CityScene.js
@@ -1,9 +1,9 @@
-import { Suspense, useState } from 'react'
+import { Suspense, useEffect, useState } from 'react'
 import { Canvas as CanvasBase } from '@react-three/fiber'
 import styled from 'styled-components'
 
 import { colors } from '@global'
-import Model from './Model'
+import Model, { Controls } from './Model'
 
 const NavItem = styled.p`
   color: ${colors.yellow};
@@ -27,32 +27,48 @@ const Canvas = styled(CanvasBase)`
   min-height: 100vh;
 `
 
-const Wrapper = styled.div``
+const Wrapper = styled.div`
+  opacity: ${p => (p.visible ? 1 : 0)};
+  transition: opacity 5000ms ease;
+`
 
 const CityScene = () => {
   const [cameraValues, setCameraValues] = useState({
-    position: [0, 0, 40800],
-    rotation: [0, 0, 0],
+    position: [-50000, -15000, 40800],
+    rotation: [0, -1.1, 0],
   })
+  const [objectLoaded, setObjectLoaded] = useState(false)
   const [newCameraValues, setNewCameraValues] = useState(cameraValues)
 
   const handlePosition = coordinates => {
     setNewCameraValues(coordinates)
-    setTimeout(() => setCameraValues(coordinates), 3000)
+    setTimeout(() => setCameraValues(coordinates), 5000)
   }
 
+  useEffect(() => {
+    if (objectLoaded) handlePosition({ position: [12000, 5000, 3800], rotation: [0, -1.1, 0] })
+  }, [objectLoaded])
+
   return (
-    <Wrapper>
+    <Wrapper visible={objectLoaded}>
       <Nav>
-        <NavItem onClick={() => handlePosition({ position: [0, 0, 40800], rotation: [0, 0, 0] })}>one</NavItem>
+        <NavItem onClick={() => handlePosition({ position: [12000, 5000, 3800], rotation: [0, -1.1, 0] })}>one</NavItem>
         <NavItem onClick={() => handlePosition({ position: [30000, -4000, 20800], rotation: [0, 0.85, 0] })}>two</NavItem>
         <NavItem onClick={() => handlePosition({ position: [20000, -8000, 3000], rotation: [0, 1.4, 0] })}>three</NavItem>
       </Nav>
       <Canvas camera={{ far: 50000, position: cameraValues.position }}>
         <ambientLight intensity={1} />
         <pointLight position={[10, 10, 10]} />
+        {/* <Controls /> */}
         <Suspense fallback={null}>
-          <Model newCameraValues={newCameraValues} position={[-1000, 0, 0]} rotation={[0, 0.5, 0]} />
+          <Model
+            handlePosition={handlePosition}
+            newCameraValues={newCameraValues}
+            objectLoaded={objectLoaded}
+            position={[-1000, 0, 0]}
+            rotation={[0, 0.5, 0]}
+            setObjectLoaded={setObjectLoaded}
+          />
         </Suspense>
       </Canvas>
     </Wrapper>
--- src/components/cityscape/Model.js
+++ src/components/cityscape/Model.js
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from 'react'
 import lerp from 'lerp'
 import { extend, useFrame, useThree } from '@react-three/fiber'
+import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -17,24 +18,32 @@ export const Controls = () => {
   return <orbitControls args={[camera, gl.domElement]} ref={orbitRef} />
 }
 
-const Model = ({ newCameraValues, ...props }) => {
+const Model = ({ newCameraValues, objectLoaded, setObjectLoaded, ...props }) => {
   const [model, setModel] = useState()
   const { camera } = useThree()
+  const manager = new THREE.LoadingManager()
+
+  manager.onProgress = function (item, objectLoaded, total) {
+    if (objectLoaded === total) setObjectLoaded(true)
+  }
 
   useFrame(() => {
     const { position, rotation } = newCameraValues
 
-    camera.rotation.x = lerp(camera.rotation.x, rotation[0], 0.03)
-    camera.rotation.y = lerp(camera.rotation.y, rotation[1], 0.03)
-    camera.rotation.z = lerp(camera.rotation.z, rotation[2], 0.03)
+    // if (objectLoaded) camera.
 
-    camera.position.x = lerp(camera.position.x, position[0], 0.03)
-    camera.position.y = lerp(camera.position.y, position[1], 0.03)
-    camera.position.z = lerp(camera.position.z, position[2], 0.03)
+    camera.rotation.x = lerp(camera.rotation.x, rotation[0], 0.015)
+    camera.rotation.y = lerp(camera.rotation.y, rotation[1], 0.015)
+    camera.rotation.z = lerp(camera.rotation.z, rotation[2], 0.015)
+
+    camera.position.x = lerp(camera.position.x, position[0], 0.015)
+    camera.position.y = lerp(camera.position.y, position[1], 0.015)
+    camera.position.z = lerp(camera.position.z, position[2], 0.015)
   })
 
   useEffect(() => {
-    new GLTFLoader().load('./obj/city/scene.gltf', setModel)
+    new GLTFLoader(manager).load('./obj/city/scene.gltf', setModel)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return model ? <primitive {...props} object={model.scene} /> : null
