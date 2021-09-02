89a3fffa64bc52d0f2b77abd27dcd462744ef5d1
Josh Kincheloe <jkincheloe33@gmail.com>
2021-09-02 13:36:34 -0500

    New sign

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Text } from '@react-three/drei'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
@@ -6,6 +7,7 @@ import { colors } from '@global'
 
 const Billboard = ({ ...props }) => {
   const [model, setModel] = useState()
+  const [sign, setSign] = useState()
   const manager = new THREE.LoadingManager()
 
   manager.onProgress = function (item, objectLoaded, total) {
@@ -14,15 +16,20 @@ const Billboard = ({ ...props }) => {
 
   useEffect(() => {
     new GLTFLoader(manager).load('./obj/billboard/scene.gltf', setModel)
+    new GLTFLoader().load('./obj/pain/scene.gltf', setSign)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  return model ? (
+  return model && sign ? (
     <>
       <primitive {...props} object={model.scene} />
+      <primitive object={sign.scene} position={[11600, -5000, 1000]} rotation={[0, 0.47, 0]} scale={3} />
       <mesh position={[7650, -2350, 1875]} rotation={[0, 0.48, 0]}>
         <planeGeometry args={[5000, 2200]} />
-        <meshBasicMaterial color={colors.berry} />
+        <meshBasicMaterial color={colors.black} />
+        <Text color='white' fontSize={500} maxWidth={4000} textAlign='center'>
+          you'll never have to leave
+        </Text>
       </mesh>
     </>
   ) : null
