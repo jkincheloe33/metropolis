a11d244824f49097c3ad4fcbce5d6457d8ada6be
Josh Kincheloe <jkincheloe33@gmail.com>
2021-12-22 08:28:44 -0700

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -14,4 +14,9 @@ module.exports = nextImages({
 
     return config
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 })
